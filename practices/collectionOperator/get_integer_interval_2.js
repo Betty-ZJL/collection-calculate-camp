5b538840e81f57ec0b622ffaff7d0c51428a87c2
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-25 18:06:19 +0800

    Update get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,17 +1,22 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-    var result=[];
-    if(number_a>number_b){
-        let c=number_a;
-        number_a=number_b;
-        number_b=c;
-    }
-    if(number_a%2!=0)
-        number_a++;
-    while(number_a>number_b)
-    return result;
-}
+  var result=[];
+	if(number_a<=number_b){
+		if(number_a%2!=0)
+			number_a++;
+		for(i=number_a;i<=number_b;i=i+2)
+			if(i%2==0)
+				result.push(i);
+	}
+	else{
+		if(number_a%2!=0)
+			number_a--;
+		for(i=number_a;i>=number_b;i=i-2)
+			if(i%2==0)
+				result.push(i);
+	}
+	return result;	
 }
 
 module.exports = get_integer_interval_2;
