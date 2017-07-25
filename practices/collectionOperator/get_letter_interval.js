e4f4171f52bcbd387b37920bf406ed9c9f785396
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-25 18:56:16 +0800

    Update get_letter_interval.js

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+    var result=[String.fromCharCode(number_a+96)];
+    while(number_a!=number_b){
+        if(number_a<=number_b)
+            result.push(String.fromCharCode((++number_a)+96));
+        else
+            result.push(String.fromCharCode((--number_a)+96));
+    }
+    return result;
 }
 
 module.exports = get_letter_interval;
