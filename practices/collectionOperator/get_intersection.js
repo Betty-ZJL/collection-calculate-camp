36d77bf5392ec315501baea10d04ae5368692989
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-25 18:31:36 +0800

    Update get_intersection.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  var result=[];
+	for(let i=0;i<collection_b.length;i++)
+		for(let j=0;j<collection_a.length;j++)
+			if(collection_a[j]==collection_b[i]){
+				result.push(collection_b[i]);
+				break;
+			}
+	return result;
 }
 
 module.exports = get_intersection;
