b24016122c57df9d289a263fc23ff461392f25d9
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-26 11:44:09 +0800

    Update get_letter_interval_2.js

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,31 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var c,y;
+  var flag=0;
+  var result=[];
+  if(number_a>number_b){
+    y=number_a;
+    number_a=number_b;
+    number_b=y;
+    flag=1;
+  }
+  for(var i=number_a;i<=number_b;i++){
+    var letters=[];
+    c=i;
+    while(c!=0){
+      y=c%26;
+      if (y==0) {y=26;c--;}
+      letters.push(String.fromCharCode(y+96));
+      c=parseInt(c/26)
+    }
+    letters.reverse();
+    result.push(letters.join(''));
+  }
+  if(flag)
+    return result.reverse();
+  else
+    return result;
 }
 
 module.exports = get_letter_interval_2;
