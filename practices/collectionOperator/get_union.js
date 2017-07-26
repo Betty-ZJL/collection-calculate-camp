c58777e716bf60b7401220b23183ab1e9c99d320
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-26 11:59:16 +0800

    Update get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  for(var i=0;i<collection_b.length;i++){
+    if (!collection_a.includes(collection_b[i]))
+      collection_a.push(collection_b[i]);
+  }
+  return collection_a;
 }
 
 module.exports = get_union;
